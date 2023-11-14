139eb00d4ee9e7cad123769042df7d17415e00bc
Yoshinori Kanno <yoshinori.kanno@me.com>
2023-11-14 18:10:02 +0900

    ✨ Navigation 設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import jaTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider, Page, Card, Button, Frame, Navigation } from '@shopify/polaris';
+import {
+  ArrowLeftMinor,
+  HomeMajor,
+  OrdersMajor,
+  ConversationMinor,
+  CashYenMajor,
+  QuestionMarkMajor,
+  ReportsMajor,
+
+} from '@shopify/polaris-icons';
+
+const navigationMarkup = (
+  <Navigation location="/">
+    <Navigation.Section
+      items={[
+        {
+          label: 'Back to Shopify',
+          icon: ArrowLeftMinor,
+        },
+      ]}
+    />
+    <Navigation.Section
+      separator
+      title="Jaded Pixel App"
+      items={[
+        {
+          label: 'ダッシュボード',
+          icon: HomeMajor,
+        },
+        {
+          label: 'ポイント設定',
+          icon: OrdersMajor,
+        },
+        {
+          label: '広告配信設定',
+          icon: OrdersMajor,
+        },
+        {
+          label: 'レポート',
+          icon: ReportsMajor,
+        },
+        {
+          label: '料金プラン',
+          icon: CashYenMajor,
+        },
+        {
+          label: 'マニュアル',
+          icon: QuestionMarkMajor,
+        },
+      ]}
+    />
+  </Navigation>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider i18n={jaTranslations}>
+      <Frame navigation={navigationMarkup}>
+
+        <Page title="Example app">
+          <Card>
+            <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+          </Card>
+        </Page>
+      </Frame>
+    </AppProvider>
   );
 }
 
